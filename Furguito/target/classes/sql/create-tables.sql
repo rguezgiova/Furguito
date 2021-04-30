4bb74e72ba11ad63c45a674b437241498f7c538f
Nefta <Neftarguez@gmail.com>
2021-04-30 12:53:17 +0100

    Se crean las tablas de la BD

--- Furguito/target/classes/sql/create-tables.sql
+++ Furguito/target/classes/sql/create-tables.sql
@@ -1,11 +1,47 @@
 CREATE TABLE IF NOT EXISTS 'Equipos' (
-    'nombre' varchar(50) NOT NULL UNIQUE,
+    'idEquipo' int(2), NOT NULL UNIQUE,
+    'nombre' varchar(50) NOT NULL,
     'ciudad' varchar(50) NOT NULL,
     'estadio' varchar(50) NOT NULL,
     'fundacion' int(4), 
     'numero_socios' int(8) DEFAULT 0,
     'presupuesto' int(10),
     'colores' varchar(50) NOT NULL,
-    PRIMARY KEY ('nombre'),
+    PRIMARY KEY ('idEquipo'),
 )
+
+CREATE TABLE IF NOT EXISTS 'Estadios' (
+    'idEstadio' int(2), NOT NULL UNIQUE
+    'nombre' varchar(50) NOT NULL,
+    'equipo' varchar(50) NOT NULL,
+    'capacidad' int(6) DEFAULT 0, 
+    'construccion' int(4) DEFAULT 0,
+    PRIMARY KEY ('idEstadio')
+)
+
+CREATE TABLE IF NOT EXISTS 'Palmares' (
+    'idPalmares' int(2), NOT NULL UNIQUE
+    'equipo' varchar(50) NOT NULL,
+    'ligas' int(2),
+    'copasDelRey' int(2),
+    'superEspana' int(2), 
+    'superEuropa' int(2),
+    'champions' int(2),
+    'mundialClubs' int(2),
+    PRIMARY KEY ('idPalmares')
+)
+
+CREATE TABLE IF NOT EXISTS 'Plantillas' (
+    'idPlantilla' int(2), NOT NULL UNIQUE
+    'equipo' varchar(50) NOT NULL,
+    'nombre' varchar(50) NOT NULL,
+    'dorsal' int(2) NOT NULL,
+    'goles' int(2) DEFAULT 0, 
+    'asistencias' int(2) DEFAULT 0,
+    'amarillas' int(2) DEFAULT 0,
+    'rojas' int(2) DEFAULT 0,
+    PRIMARY KEY ('idPalmares')
+)
+
+
 
