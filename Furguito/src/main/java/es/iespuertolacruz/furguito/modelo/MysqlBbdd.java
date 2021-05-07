21ccd8213cbd94cb581812a0136dd74fe863c58f
rguezgiova <grodriguez.inf@gmail.com>
2021-05-07 17:25:53 +0100

    Se crea la clase MySqlBbdd

---
+++ Furguito/src/main/java/es/iespuertolacruz/furguito/modelo/MysqlBbdd.java
@@ -0,0 +1,9 @@
+package es.iespuertolacruz.furguito.modelo;
+
+public class MysqlBbdd extends Bbdd {
+
+    public MysqlBbdd(String driver, String url, String usuario, String password) {
+        super(driver, url, usuario, password);
+    }
+
+}
