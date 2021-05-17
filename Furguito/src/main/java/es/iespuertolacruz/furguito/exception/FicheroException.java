4cf736fde7275bc954df51668258dfa0b0bea2e9
Nefta <Neftarguez@gmail.com>
2021-05-17 15:58:36 +0100

    #19 Creada la clase fichero

---
+++ Furguito/src/main/java/es/iespuertolacruz/furguito/exception/FicheroException.java
@@ -0,0 +1,26 @@
+package es.iespuertolacruz.furguito.exception;
+
+public class FicheroException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructor con mensaje personalizado
+     * 
+     * @param mensaje personalizado
+     */
+    public FicheroException(String mensaje) {
+        super(mensaje);
+    }
+
+    /**
+     * Constructor con mensaje y la excepcion que se produce
+     * 
+     * @param mensaje   mensaje de la excepcion
+     * @param exception que produce el error
+     */
+    public FicheroException(String mensaje, Exception exception) {
+        super(mensaje, exception);
+    }
+
+}
