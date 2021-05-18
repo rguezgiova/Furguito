48fa2238a466ca1ebf2fea2a40cc1102a232541c
Nefta <Neftarguez@gmail.com>
2021-05-18 16:59:23 +0100

    cambios en la clase sqlite

--- Furguito/src/main/java/es/iespuertolacruz/furguito/modelo/SqliteBbdd.java
+++ Furguito/src/main/java/es/iespuertolacruz/furguito/modelo/SqliteBbdd.java
@@ -1,9 +1,18 @@
 package es.iespuertolacruz.furguito.modelo;
 
+import es.iespuertolacruz.furguito.exception.PersistenciaException;
+
 public class SqliteBbdd extends Bbdd {
 
-    public SqliteBbdd(String driver, String url, String usuario, String password) {
+    private static final String DRIVER = "org.sqlite.JDBC";
+    private static final String URL = "jdbc:sqlite:furguito.db";
+
+    public SqliteBbdd(String driver, String url, String usuario, String password) throws PersistenciaException {
         super(driver, url, usuario, password);
+    }
+
+    public SqliteBbdd(String usuario, String password) throws PersistenciaException {
+        super(DRIVER, URL, usuario, password);
     }
 
 }
